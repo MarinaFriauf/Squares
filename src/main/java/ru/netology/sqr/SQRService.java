e73ac343a0c26c530667a58fccac2bb0248222b0
MarinaFriauf <marina506506506@gmail.com>
2022-03-30 05:21:14 +0700

    finish

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,13 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int calculate(int min, int max) {
+        int counter = 0;
+        for (int i = 10; i < 100; i++) {
+            if (i * i > min && i * i < max) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQRServiceTest {
+    @ParameterizedTest
+    @CsvSource(value = {"several in diapason, 200, 300, 3",
+            "zero in diapason, 0, 100, 0",
+            "one in diapason, 0, 101, 1",
+            "all in diapason, 0, 100000000, 90"}
+    )
+    void shouldCalculate(String testName, int min, int max, int expected) {
+
+        SQRService service = new SQRService();
+
+        //целевой метод
+        int actual = service.calculate(min, max);
+
+        assertEquals(expected, actual);
+
+    }
+
+}
